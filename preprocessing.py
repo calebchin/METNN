33ce264a9fe137b295d5a11d5e201cabf2838d3b
akl6463 <143304195+akl6463@users.noreply.github.com>
2024-12-02 20:55:35 -0500

    Update preprocessing.py
    Added edge length functionality

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Bio import Phylo
 
 """
 
@@ -26,9 +27,62 @@ def get_empirical (table):
     sum_sample = 0
     for species in range(len(data_matrix)):
       sum_sample+=data_matrix[species][sample]
-    sample_est = []
+    sample_est = {}
     for species in range(len(data_matrix)):
-      sample_est.append(data_matrix[species][sample]/sum_sample)
+      OTU = (table.ids('observation'))[species]
+      sample_est[OTU] = (data_matrix[species][sample]/sum_sample)
     empirical.append(sample_est)
 
+
+    #Convert empirical into dict, access edges to multiply by L using descendants
+
     return empirical
+  
+
+"""
+Returns a list of all the edge lengths in the tree, along with, a list of lists
+of empirical distributions of edges, one for each sample. The lists of empirical 
+distributions are in the same order as the list of edge lengths.
+
+Note that as defined in the paper, an empirical distribution of an edge is the 
+sum of the empirical distributions of all nodes whose paths from the root node 
+pass through said edge.
+"""
+  
+def empirical_dist_edge_len (tree, table):
+
+  empirical_dist = get_empirical(table)
+  num_samples = len(empirical_dist)
+  loaded_tree = Phylo.read(tree, "newick")
+
+  Ls = []
+  dists = []
+
+  for _ in range(num_samples):
+    dists.append([])
+
+  for node in loaded_tree.find_clades():
+    if node.branch_length is not None:
+      Ls.append(node.branch_length)
+      descendants = [node]+list(node.find_clades())
+
+      for i in range(num_samples):
+        tot = 0
+        for OTU in descendants:
+          tot+=empirical_dist[i][OTU]         
+        dists[i].append(tot)
+
+    else:
+      continue
+
+  return Ls, dists
+
+
+
+  #Loop through edges of phylo tree object
+
+  #for each edge, calculate empirical distribution (=sum of other empirical distributions)
+  
+
+
+
